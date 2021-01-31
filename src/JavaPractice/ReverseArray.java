7eeadf7ef23f1d5bcd20cbbf1dd532ad039b3099
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2021-01-31 17:09:51 +0530

    reverse the given array

--- src/JavaPractice/ReverseArray.java
+++ src/JavaPractice/ReverseArray.java
@@ -1,8 +1,23 @@
 package JavaPractice;
 
+import java.util.Scanner;
+
 public class ReverseArray {
     public static void main(String[] args) {
-        System.out.println("hello ");
-        System.out.println("usfhuwkiw");
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[5];
+        for (int i = 0; i <array.length ; i++) {                              //loop for entering values to array
+            System.out.println("Enter the element");
+            array[i] = scanner.nextInt();
+        }
+        System.out.println("Original Array:");
+        for (int i = 0; i <array.length ; i++) {                                 //original array
+            System.out.print(array[i]+" ");
+        }
+        System.out.println();
+        System.out.println("Reversed Array:");
+        for (int i = array.length-1; i >=0 ; i--) {                                     //reversed array
+            System.out.print(array[i]+" ");
+        }
     }
 }
