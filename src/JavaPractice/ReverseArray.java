390633d8eaa7aabcff7d3a0a5f04358eef6423b6
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2021-01-31 16:59:39 +0530

    reverse the given array

--- src/JavaPractice/ReverseArray.java
+++ src/JavaPractice/ReverseArray.java
@@ -3,5 +3,6 @@ package JavaPractice;
 public class ReverseArray {
     public static void main(String[] args) {
         System.out.println("hello ");
+        System.out.println("usfhuwkiw");
     }
 }
