cda1703a6b788a19a9eee4e77105a0c7db1769c5
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2021-01-31 16:45:49 +0530

    reverse the array

--- src/JavaPractice/ReverseArray.java
+++ src/JavaPractice/ReverseArray.java
@@ -1,2 +1,7 @@
-package JavaPractice;public class ReverseArray {
+package JavaPractice;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        System.out.println("hello ");
+    }
 }
