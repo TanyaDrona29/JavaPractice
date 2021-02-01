abfd8cfed19cd32e6b31d2b5bff42e4c026e3504
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2021-02-01 13:02:34 +0530

    maximum and minimum values in a given array

---
+++ src/JavaPractice/MaxAndMin.java
@@ -0,0 +1,29 @@
+package JavaPractice;
+
+import java.util.Scanner;
+
+public class MaxAndMin {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        int max =0;
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i <array.length; i++) {                                   //entering values in array
+            System.out.println("enter element");
+            array[i] = scanner.nextInt();
+        }
+        System.out.println();
+        System.out.println("original array:");
+        for (int i = 0; i <array.length; i++) {                                     //printing the original array
+            System.out.print(array[i]+" ");
+        }
+        System.out.println();
+        for (int i = 0; i < array.length; i++) {                                      //finding the maximum value
+            if (max < array[i]){
+                max = array[i];
+            }
+
+        }
+        System.out.println("max is:");                                           //printing the max number
+        System.out.println(max);
+    }
+}
