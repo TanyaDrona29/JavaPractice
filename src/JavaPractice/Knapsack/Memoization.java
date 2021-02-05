7d07ee7d0b5ce1fa153b884f44bedb7a73e5762b
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2021-02-05 16:41:25 +0530

    knapsack code with memoization

---
+++ src/JavaPractice/Knapsack/Memoization.java
@@ -0,0 +1,29 @@
+package JavaPractice.Knapsack;
+
+public class Memoization {
+        static int max(int a, int b)
+        {
+            return (a > b) ? a : b;
+        }
+        static int knapSack(int W, int wt[], int val[], int n)
+        {
+            if (n == 0 || W == 0)
+                return 0;
+            if (wt[n - 1] > W)
+                return knapSack(W, wt, val, n - 1);
+            else
+                return max(val[n - 1]
+                                + knapSack(W - wt[n - 1], wt,
+                        val, n - 1),
+                        knapSack(W, wt, val, n - 1));
+        }
+        public static void main(String args[])
+        {
+            int val[] = new int[] { 1,4,5,7 };
+            int wt[] = new int[] { 1,3,4,5 };
+            int W = 7;
+            int n = val.length;
+            System.out.println(knapSack(W, wt, val, n));
+        }
+    }
+
