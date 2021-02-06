414edf7160448deb6ef9695c5f4ddbc39c48c564
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2021-02-06 17:19:35 +0530

    knapsack code with memoization

--- src/JavaPractice/Knapsack/Memoization.java
+++ src/JavaPractice/Knapsack/Memoization.java
@@ -1,29 +1,39 @@
 package JavaPractice.Knapsack;
 
-public class Memoization {
-        static int max(int a, int b)
-        {
-            return (a > b) ? a : b;
-        }
-        static int knapSack(int W, int wt[], int val[], int n)
-        {
-            if (n == 0 || W == 0)
-                return 0;
-            if (wt[n - 1] > W)
-                return knapSack(W, wt, val, n - 1);
-            else
-                return max(val[n - 1]
-                                + knapSack(W - wt[n - 1], wt,
-                        val, n - 1),
-                        knapSack(W, wt, val, n - 1));
-        }
-        public static void main(String args[])
-        {
-            int val[] = new int[] { 1,4,5,7 };
-            int wt[] = new int[] { 1,3,4,5 };
-            int W = 7;
-            int n = val.length;
-            System.out.println(knapSack(W, wt, val, n));
-        }
+public class Memoization {                                               //max function to find out the maximun value
+    static int max(int a, int b){
+        return(a>b)?a:b;
     }
+    static int knapsack(int[] weight, int[] value, int bag , int arraysize){
+        int matrix[][] = new int[arraysize+1][bag+1];
+        for (int i = 0; i <arraysize; i++) {                         //initialize matrix with -1
+            for (int j = 0; j <bag ; j++) {
+                matrix[i][j]= -1;
+            }
+        }
+        //check if either bag capacity is zero or if array is empty
+        if(arraysize ==0 || bag == 0){
+            return 0;
+        }
+        if(matrix[arraysize][bag] !=0){
+            return matrix[arraysize][bag];
+        }
+        if(weight[arraysize-1]<=bag){
+            return matrix[arraysize][bag]= max(value[arraysize-1]+knapsack(weight,value,bag-weight[arraysize-1],arraysize-1),
+                    knapsack(weight,value,bag,arraysize-1));
+        }
+        else
+            return matrix[arraysize][bag]= knapsack(weight,value,bag,arraysize-1);
+    }
+    public static void main(String[] args) {
+
+        int[] weight = {1, 2, 3, 5};
+        int[] value = {2,3,4,5};
+        int bag= 6;
+        int arraysize = weight.length;
+        System.out.println("max profit is:"+knapsack(weight,value, bag,arraysize));
+    }
+
+
+}
 
