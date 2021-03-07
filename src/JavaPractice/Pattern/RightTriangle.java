b298e0b721b9f033f31da1120fdde2b74fbed25f
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2021-03-07 11:53:18 +0530

    code to print pattern in right right angled triange shape

--- src/JavaPractice/Pattern/RightTriangle.java
+++ src/JavaPractice/Pattern/RightTriangle.java
@@ -7,11 +7,11 @@ public class RightTriangle {
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter the size");
         int size = scanner.nextInt();
-        for (int i =1; i<=size+1; i++) {
-            for (int j = 1; j <=i; j++) {
+        for (int i = 1; i <= size + 1; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("  ");
             }
-            for (int j =0; j <size-i+1 ; j++) {
+            for (int j = 0; j < size - i + 1; j++) {
                 System.out.print("* ");
             }
             System.out.println();
