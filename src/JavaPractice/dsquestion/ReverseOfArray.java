9f10a71b7582d7cd5d34fba05d17ee14252cfb39
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2021-03-25 10:16:15 +0530

    reverse of array

---
+++ src/JavaPractice/dsquestion/ReverseOfArray.java
@@ -0,0 +1,11 @@
+package JavaPractice.dsquestion;
+
+public class ReverseOfArray {
+    public static void main(String[] args) {
+        int array[] = {1,2,3,4,5};
+        int size = array.length;
+        for (int i = size-1; i>0 ; i--) {
+            System.out.print(array[i]+" ");
+        }
+    }
+}
