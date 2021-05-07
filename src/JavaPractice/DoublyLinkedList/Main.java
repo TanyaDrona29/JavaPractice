1875cf8ca26ad956ed88f3d31371ab0226a44b05
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2021-05-07 12:27:58 +0530

    main class for doubly linked list

---
+++ src/JavaPractice/DoublyLinkedList/Main.java
@@ -0,0 +1,12 @@
+package JavaPractice.DoublyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        Definition definition = new Definition();
+        definition.insertAtBeg(40);
+        definition.insertAtBeg(30);
+        definition.insertAtBeg(20);
+        definition.insertAtBeg(10);
+        definition.printList();
+    }
+}
