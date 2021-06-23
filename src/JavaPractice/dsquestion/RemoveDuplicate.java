3eb404c095aadd333eb288d4891b7ddfd00a048e
TanyaDrona29 <tanya.drona_ccv18@gla.ac.in>
2021-06-23 09:00:39 +0530

    remove duplicate elements in an array

---
+++ src/JavaPractice/dsquestion/RemoveDuplicate.java
@@ -0,0 +1,17 @@
+package JavaPractice.dsquestion;
+
+import java.util.LinkedHashSet;
+
+public class RemoveDuplicate {
+    public static void remove(int[] arr){
+        LinkedHashSet<Integer>hashSet = new LinkedHashSet<Integer>();
+        for (int i = 0; i < arr.length ; i++) {
+            hashSet.add(arr[i]);
+        }
+        System.out.println(hashSet);
+    }
+    public static void main(String[] args) {
+        int array[] = {10,32,4445,121,33,10, 32,4445};
+        remove(array);
+    }
+}
